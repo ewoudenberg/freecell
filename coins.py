5cae584e6e84d0b6a99e8d658ac2df073acc3b4c
Lawrence E. Bakst <leb@me.com>
2019-03-13 15:56:23 -0400

    update with eaw's suggestions

--- coins.py
+++ coins.py
@@ -3,37 +3,28 @@ import random
 
 # Coins keep track of the the number of heads and tails they've had
 class Coin:
-	def headp(self):
-		odds = random.random()
-		if odds <= self.head_odds:
-			return True
-		else:
-			return False
-
-	def flip(self):
-		odds = random.random()
-		if self.headp():
-			self.heads += 1
-		else:
-			self.tails += 1
-		return self.heads, self.tails
-
-	def flips(self, n=1):
-		for i in range(n):
-			self.flip()
-		return self.heads, self.tails
-
 	def __init__(self, head_odds=0.5):
 		self.head_odds = head_odds
 		self.heads = 0
 		self.tails = 0
 
+	def headp(self):
+		return random.random() <= self.head_odds
+
+	def flip(self, n=1):
+		print(n)
+		for i in range(n):
+			if self.headp():
+				self.heads += 1
+			else:
+				self.tails += 1
+		return self.heads, self.tails
+
 # A CoinSet is a list of coins that all have the same fairness
 class CoinSet:
 	def __init__(self, head_odds=0.5, n=10):
-		self.coins = []
 		for x in range(n):
-			self.coins.append(Coin(head_odds))
+			self.coins = [ Coin(head_odds) for x in range(n) ]
 
 # trial flips all the coins in the coinset and returns the number of heads and tails
 # see the binomial theorem at work
