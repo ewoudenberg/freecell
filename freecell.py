3d5d9e9b559be3ad53ff609170bd409d9a4f6598
ewoudenberg-kasisto <eric.woudenberg@kasisto.com>
2019-03-24 20:49:08 -0400

    fix potential bug in get_column_move_size

--- freecell.py
+++ freecell.py
@@ -139,7 +139,7 @@ class Column(list):
     # the number of cards it involves. Return 0 if there isn't one.
     def get_column_move_size(self, src_column, supermove_room):
         src_run_length = src_column.get_final_run_length()
-        max_cards = min(supermove_room, src_run_length, self.max_length)
+        max_cards = min(supermove_room, src_run_length, self.max_length-len(self))
         # Loop through possible xfers (trying the largest stretch of cards first
         # since moves to an empty column can start from any card in the string).
         for i in range(max_cards, 0, -1):
@@ -180,7 +180,7 @@ class Column(list):
             return self[-1-depth]
 
 # A ColumnGroup is a unifying container for the tableau, frees 
-# and homes collections of columns
+# and homes collections of columns.
 # The constructor takes a list of columns.
 
 class ColumnGroup(list):
