5edf77ecbdbc535ea19c7999fa0310f53221876e
ewoudenberg-kasisto <eric.woudenberg@kasisto.com>
2019-03-26 05:35:14 -0400

    use float("Inf") for the non freecell deck size

--- freecell.py
+++ freecell.py
@@ -78,13 +78,13 @@ class Card:
     def __repr__(self): # for debugging
         return f'Card: suit={self.suit} ({self.glyph}) rank={self.rank}'
     
-# Columns are used to implement free cells, suit homes, and columns in the tableau.
+# Columns are used to implement free cells, suit homes, and cascades in the tableau.
 
 class Column(list):
     def __init__(self, type=None, location=''):
         type_configurations = dict(FREECELL=dict(max_length=1, cascade=True),
-                                   HOME=dict(max_length=DECK_SIZE, cascade=False),
-                                   TABLEAU=dict(max_length=DECK_SIZE, cascade=True))
+                                   HOME=dict(max_length=float('Inf'), cascade=False),
+                                   TABLEAU=dict(max_length=float('Inf'), cascade=True))
 
         if type not in type_configurations:
             raise Exception(f'Column __init__ botch: unknown type "{type}"')
