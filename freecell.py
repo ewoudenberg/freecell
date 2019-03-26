92f10f3dc2401c7bda9c42a07c10976ea3873ff4
ewoudenberg-kasisto <eric.woudenberg@kasisto.com>
2019-03-26 12:01:17 -0400

    cleanup

--- freecell.py
+++ freecell.py
@@ -11,22 +11,22 @@ Construction and layout
 
 - One standard 52-card deck is used.
 - There are four open cells and four open foundations. Some alternate rules use between one and ten cells.
-= Cards are dealt face-up into eight cascades, four of which comprise seven cards each and four of which 
+- Cards are dealt face-up into eight cascades, four of which comprise seven cards each and four of which 
   comprise six cards each. Some alternate rules will use between four and ten cascades.
 
 Building during play
 
-- The top card of each cascade begins a cascades.
+- The top card of each cascade begins a tableau.
 - Tableaux must be built down by alternating colors.
 - Foundations are built up by suit.
 
 Moves
-- Any cell card or top card of any cascade may be moved to build on a cascades, 
-  or moved to an empty cell, an empty cascade, or its foundation.
-- Complete or partial tableaus may be moved to build on existing tableaus, 
-  or moved to empty cascades, by recursively placing and removing cards through 
-  intermediate locations. Computer implementations often show this motion, but players using 
-  physical decks typically move the cascades at once.
+
+- Any cell card or top card of any cascade may be moved to build on a tableau, or moved to an empty cell, 
+  an empty cascade, or its foundation.
+- Complete or partial tableaus may be moved to build on existing tableaus, or moved to empty cascades, by 
+  recursively placing and removing cards through intermediate locations. Computer implementations often show 
+  this motion, but players using physical decks typically move the tableau at once.
 '''
 
 import random
