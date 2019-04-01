f967ff17be8831c327767deff718322a112dc76c
ewoudenberg-kasisto <eric.woudenberg@kasisto.com>
2019-03-31 22:58:32 -0400

    Disallow freecell-to-frecell moves

--- freecell.py
+++ freecell.py
@@ -162,7 +162,8 @@ class Column(list):
     # Can some cards from the given column be added to this column, given the amount
     # of movement room?
     def can_accept_column(self, src_column, movement_room):
-        return self.get_column_move_size(src_column, movement_room) != 0
+        freecell_to_freecell = self.type == 'FREECELL' and src_column.type == 'FREECELL'
+        return not freecell_to_freecell and self.get_column_move_size(src_column, movement_room) != 0
 
     # Find a legal move from the src column into ours and report
     # the number of cards it involves. Return 0 if there isn't one.
